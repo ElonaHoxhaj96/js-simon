e5769885a8770e851414f1ef034d0eadffdd5306
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-17 21:47:38 +0200

    feat: generate function

---
+++ function.js
@@ -0,0 +1,27 @@
+for (let i = 0; i < 5; i++){
+    //funzione con numeri casuali
+    function numeroRandomico (){
+        numeroGenerico =  Math.floor( Math.random() * 100);
+        return numeroGenerico;
+    }
+    console.log(numeroRandomico(i));
+}
+/// Funzione che genera un nn numero random 
+function generaNumRand(min, max){
+    let numGenerato = Math.floor(Math.random() * (max - min + 1) + min);
+    return numGenerato;
+}
+
+// Funzione genera numeri random all'inrerno di un range prestabilito 
+function randomicoDentroRage (min , max, nrLenght){
+    const numeriArray = [];
+    // creo un ciclo che mi generi i numeri in base a quanti ne ho bisogno 
+    while(numeriArray.length < nrLenght){
+        let funzGeneraRandom = generaNumRand7(min, max);
+        // se all'interno del mio array è present il numero generato non lo includo, altrimewnti lo pusho all'interno dell' array
+        if(!numeriArray.includes(funzGeneraRandom));
+        numeriArray.push(funzGeneraRandom);
+    }
+
+}
+
--- main.js
+++ main.js
@@ -1,18 +1,26 @@
 //creo un ciclo che mi svolga qualcosa per 5 volte 
-for (let i = 0; i < 5; i++){
-    //funzione con numeri casuali
-    function numeroRandomico (){
-        numeroGenerico =  Math.floor( Math.random() * 100);
-        return numeroGenerico;
-    }
-    console.log(numeroRandomico(i));
-    //selezione il contenitore all'interno del quale stampero in numeri che poi scompariranno 
-let contenitore =document.getElementById('contenitor');
-let numeri = `<div class="nr_generato">${numeroRandomico(i)}</div>`;
-contenitore.innerHTML += numeri;
-}
-
+let 
 
 //impostare un timer di 30 dopo i quali i numeri casuali scompaiono
 //chieder all'utente di inserire i numeri che ha visto poco prima una volta tramiter un prompt
 // restituire il risultato di quelli giusti 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let contenitore =document.getElementById('contenitor');
+// let numeri = `<div class="nr_generato">${numeroRandomico(i)}</div>`;
+// contenitore.innerHTML += numeri;
