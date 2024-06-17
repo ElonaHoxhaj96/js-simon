14d5262134fb60810ed5ded23eb54db4b55f1349
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-17 16:47:29 +0200

    feat: greated Flowchart

---
+++ main.js
@@ -0,0 +1,7 @@
+//creo un ciclo che mi svolga qualcosa per 5 volte 
+
+    //funzione con numeri casuali
+
+    //impostare un timer di 30 dopo i quali i numeri casuali scompaiono
+    //chieder all'utente di inserire i numeri che ha visto poco prima una volta tramiter un prompt
+    // restituire il risultato di quelli giusti 
