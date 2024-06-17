58a5cc9d1a3eca66fbd58c528504d995233e68e7
ElonaHoxhaj96 <elona6915@gmail.com>
2024-06-17 22:13:13 +0200

    feat: timer set

--- function.js
+++ function.js
@@ -1,11 +1,11 @@
-for (let i = 0; i < 5; i++){
-    //funzione con numeri casuali
-    function numeroRandomico (){
-        numeroGenerico =  Math.floor( Math.random() * 100);
-        return numeroGenerico;
-    }
-    console.log(numeroRandomico(i));
-}
+// for (let i = 0; i < 5; i++){
+//     //funzione con numeri casuali
+//     function numeroRandomico (){
+//         numeroGenerico =  Math.floor( Math.random() * 100);
+//         return numeroGenerico;
+//     }
+//     console.log(numeroRandomico(i));
+// }
 /// Funzione che genera un nn numero random 
 function generaNumRand(min, max){
     let numGenerato = Math.floor(Math.random() * (max - min + 1) + min);
@@ -17,11 +17,11 @@ function randomicoDentroRage (min , max, nrLenght){
     const numeriArray = [];
     // creo un ciclo che mi generi i numeri in base a quanti ne ho bisogno 
     while(numeriArray.length < nrLenght){
-        let funzGeneraRandom = generaNumRand7(min, max);
+        let funzGeneraRandom = generaNumRand(min, max);
         // se all'interno del mio array è present il numero generato non lo includo, altrimewnti lo pusho all'interno dell' array
         if(!numeriArray.includes(funzGeneraRandom));
         numeriArray.push(funzGeneraRandom);
     }
-
+    return numeriArray
 }
 
--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 //creo un ciclo che mi svolga qualcosa per 5 volte 
-let 
+let cicloCinque = randomicoDentroRage (1 , 500, 5);
+console.log(cicloCinque);
+// stampiamo i numeri in pagina 
 
+let contenitore =document.getElementById('contenitor');
+let numeri = document.createElement('div');
+numeri.innerHTML = cicloCinque;
+contenitore.append(numeri);
 //impostare un timer di 30 dopo i quali i numeri casuali scompaiono
+setTimeout(nascondi, 3000)
+//creo una funzione per rendere il mio elemento nascosto 
+function nascondi(){
+    numeri.classList.add('hiden');
+}
+
+
 //chieder all'utente di inserire i numeri che ha visto poco prima una volta tramiter un prompt
 // restituire il risultato di quelli giusti 
 
@@ -21,6 +34,3 @@ let
 
 
 
-// let contenitore =document.getElementById('contenitor');
-// let numeri = `<div class="nr_generato">${numeroRandomico(i)}</div>`;
-// contenitore.innerHTML += numeri;
